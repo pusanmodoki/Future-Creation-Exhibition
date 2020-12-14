81bafd633e1496a98325b6639ec090c8ed035408
roze <wa.tf.384.7o5og@gmail.com>
2020-12-14 14:10:08 +0900

    敵の処理　途中３

---
+++ MisotempraProject/Assets/Scenes/PracticeYamada/script/Enemy_Ini.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using AI.BehaviorTree;
+using UnityEngine;
+
+public class Enemy_Ini : AI.BehaviorTree.BaseBlackboardInitializer
+{
+
+    [SerializeField]
+    string m_animationAttackKey = null;
+    int m_animationID;
+
+    public override void InitializeAllInstance(Blackboard blackboard)//
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void InitializeFirstInstance(Blackboard blackboard)//最初のインスタンス
+    {
+        throw new NotImplementedException();
+    }
+}

d35921a58af3688de394840a15db9f272498da66
pusanmodoki <blackdragon.chanp@gmail.com>
2020-12-15 15:58:16 +0900

    KubiItai

--- MisotempraProject/Assets/Scripts/AI/Decorators/DiscoverPlayer.cs
+++ MisotempraProject/Assets/Scripts/AI/Decorators/DiscoverPlayer.cs
@@ -11,9 +11,12 @@ namespace AI
 			[System.Serializable]
 			public class DiscoverPlayer : BaseDecorator
 			{
+				[SerializeField, Tooltip("ビット反転")]
+				bool m_isBitInversion = false;
+
 				public override bool IsPredicate(AIAgent agent, Blackboard blackboard)
 				{
-					return agent.vision ? agent.vision.isDiscoverStay : false;
+					return agent.vision ? agent.vision.isDiscoverStay ^ m_isBitInversion: false;
 				}
 			}
 		}
